7d4ecc9b2858b2855265d3b84b90670884a43d26
Neil Kline <lolnk361@gmail.com>
2018-10-04 06:10:46 -0400

    Hotfix: fixed rebalance in remove item in avl int binary tree
    Rebalancing before was only getting called on the parent for all delete routes, but two routes needed to call it on the current nodes first, then the parents on up.

--- D_S_and_A/c_AVL_int_binary_tree.h
+++ D_S_and_A/c_AVL_int_binary_tree.h
@@ -63,11 +63,21 @@ public:
 			{
 				delete *ancestors[ancestors.size() - 1];
 				*ancestors[ancestors.size() - 1] = nullptr;
+				for (int i = ancestors.size() - 1 - 1; i >= 0; --i)//i must be int type, size_t cannot go less than zero, - 2 to start at first possible grandparent
+			                rebalance(ancestors[i]);
 			}
 			else if ((*ancestors[ancestors.size() - 1])->right == nullptr)//only one child node exists, so just overwrite *found with that child
+			{
 				*ancestors[ancestors.size() - 1] = (*ancestors[ancestors.size() - 1])->left;
+				for (int i = ancestors.size() - 1; i >= 0; --i)//i must be int type, size_t cannot go less than zero, - 1 to start at first possible grandparent, current
+			                 rebalance(ancestors[i]);
+			}
 			else if ((*ancestors[ancestors.size() - 1])->left == nullptr)
+			{
 				*ancestors[ancestors.size() - 1] = (*ancestors[ancestors.size() - 1])->right;
+				for (int i = ancestors.size() - 1; i >= 0; --i)//i must be int type, size_t cannot go less than zero, - 1 to start at first possible grandparent, current
+			                 rebalance(ancestors[i]);
+			}
 			else//both child nodes exist
 			{
 				NodeType<int> * * current = &(*ancestors[ancestors.size() - 1])->right;//search for the leftmost node of right child
@@ -85,8 +95,8 @@ public:
 				}
 				else//move right nodes up if it was purely the right node
 					*current = (*current)->right;
+				for (int i = ancestors.size() - 1 - 1; i >= 0; --i)//i must be int type, size_t cannot go less than zero, - 2 to start at first possible grandparent
+			                rebalance(ancestors[i]);
 			}
-		for (int i = ancestors.size() - 1 - 1; i >= 0; --i)//i must be int type, size_t cannot go less than zero, - 2 to start at first possible grandparent
-			rebalance(ancestors[i]);
 	}
 };
